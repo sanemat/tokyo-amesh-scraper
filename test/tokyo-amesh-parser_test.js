8ac23cdcba3afc8d87318b5f65bb265a7c0b3f76
sanemat <o.gata.ken@gmail.com>
2014-05-29 23:18:24 +0900

    Add semicolon

--- test/tokyo-amesh-parser_test.js
+++ test/tokyo-amesh-parser_test.js
@@ -5,12 +5,12 @@ var assert = require("power-assert");
 
 describe('amesh URL', function(){
   it('should return boundaryMap', function(){
-    assert.equal(tokyoAmeshParser.boundaryMapUrl(), 'http://tokyo-ame.jwa.or.jp/map/msk000.png')
+    assert.equal(tokyoAmeshParser.boundaryMapUrl(), 'http://tokyo-ame.jwa.or.jp/map/msk000.png');
   });
   it('should return landform', function(){
-    assert.equal(tokyoAmeshParser.landformURL(), 'http://tokyo-ame.jwa.or.jp/map/map000.jpg')
+    assert.equal(tokyoAmeshParser.landformURL(), 'http://tokyo-ame.jwa.or.jp/map/map000.jpg');
   });
   it('should return mesh', function(){
-    assert.equal(tokyoAmeshParser.rainMeshURL(), 'http://tokyo-ame.jwa.or.jp/mesh/000/201405272250.gif')
+    assert.equal(tokyoAmeshParser.rainMeshURL(), 'http://tokyo-ame.jwa.or.jp/mesh/000/201405272250.gif');
   });
 });
