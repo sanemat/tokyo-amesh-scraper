801f799be730a1bb8dab46ab6c5f2f3bf10261aa
sanemat <o.gata.ken@gmail.com>
2014-05-30 01:58:21 +0900

    Update example

--- example.js
+++ example.js
@@ -3,9 +3,9 @@ var tokyoAmesh = require('tokyo-amesh');
 var mesh = tokyoAmesh({highResolution: true});
 var mesh = tokyoAmesh();
 
-mesh.boundaryMapURL();//=> http://tokyo-ame.jwa.or.jp/map/msk000.png
-mesh.landformURL();//=> http://tokyo-ame.jwa.or.jp/map/map000.jpg
-mesh.rainMeshURL();//=> http://tokyo-ame.jwa.or.jp/mesh/000/201405272250.gif
+mesh.boundaryMapURL(function(data){});// data => http://tokyo-ame.jwa.or.jp/map/msk000.png
+mesh.landformURL(function(data){});// data => http://tokyo-ame.jwa.or.jp/map/map000.jpg
+mesh.rainMeshURL(function(data){});// data => http://tokyo-ame.jwa.or.jp/mesh/000/201405272250.gif
 
 // http://tokyo-ame.jwa.or.jp/scripts/mesh_index.js
 // or
