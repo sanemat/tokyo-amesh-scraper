60952bbd73bff1b6eaffc3a488280709f989dba9
sanemat <o.gata.ken@gmail.com>
2014-05-30 01:52:33 +0900

    Use callback style

--- test/tokyo-amesh-parser_test.js
+++ test/tokyo-amesh-parser_test.js
@@ -4,13 +4,22 @@ var tokyoAmeshParser = require('../lib/tokyo-amesh-parser.js');
 var assert = require("power-assert");
 
 describe('amesh URL', function(){
-  it('should return boundaryMap', function(){
-    assert.equal(tokyoAmeshParser.boundaryMapUrl(), 'http://tokyo-ame.jwa.or.jp/map/msk000.png');
+  it('should return boundaryMap', function(done){
+    tokyoAmeshParser.boundaryMapUrl(function(url){
+      assert.equal(url, 'http://tokyo-ame.jwa.or.jp/map/msk000.png');
+      done();
+    });
   });
-  it('should return landform', function(){
-    assert.equal(tokyoAmeshParser.landformURL(), 'http://tokyo-ame.jwa.or.jp/map/map000.jpg');
+  it('should return landform', function(done){
+    tokyoAmeshParser.landformURL(function(url){
+      assert.equal(url, 'http://tokyo-ame.jwa.or.jp/map/map000.jpg');
+      done();
+    });
   });
-  it('should return mesh', function(){
-    assert.equal(tokyoAmeshParser.rainMeshURL(), 'http://tokyo-ame.jwa.or.jp/mesh/000/201405272250.gif');
+  it('should return mesh', function(done){
+    tokyoAmeshParser.rainMeshURL(function(url){
+      assert.equal(url, 'http://tokyo-ame.jwa.or.jp/mesh/000/201405272250.gif');
+      done();
+    });
   });
 });
