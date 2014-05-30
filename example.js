683b94ec12aed4773335a3c3adbdc3d7f0f3b1ba
sanemat <o.gata.ken@gmail.com>
2014-05-31 00:25:13 +0900

    Update readme

--- example.js
+++ example.js
@@ -1,11 +1,11 @@
-var tokyoAmesh = require('tokyo-amesh');
+var tokyoAmeshScraper = require('tokyo-amesh-scraper');
 
-var mesh = tokyoAmesh({highResolution: true});
-var mesh = tokyoAmesh();
+var amesh = tokyoAmeshScraper({highResolution: true});
+var amesh = tokyoAmeshScraper();
 
-mesh.boundaryMapURL(function(data){});// data => http://tokyo-ame.jwa.or.jp/map/msk000.png
-mesh.landformURL(function(data){});// data => http://tokyo-ame.jwa.or.jp/map/map000.jpg
-mesh.rainMeshURL(function(data){});// data => http://tokyo-ame.jwa.or.jp/mesh/000/201405272250.gif
+amesh.boundaryMapURL(function(data){});// data => http://tokyo-ame.jwa.or.jp/map/msk000.png
+amesh.landformURL(function(data){});// data => http://tokyo-ame.jwa.or.jp/map/map000.jpg
+amesh.rainMeshURL(function(data){});// data => http://tokyo-ame.jwa.or.jp/mesh/000/201405272250.gif
 
 // http://tokyo-ame.jwa.or.jp/scripts/mesh_index.js
 // or
